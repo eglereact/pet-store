3178ca7e30f7d33a4b1cff213ff67ee3c7b23f48
eglereact <eglereactjs@gmail.com>
2024-10-25 15:37:52 +0300

    add auth provider

--- components/navbar/SignOut.tsx
+++ components/navbar/SignOut.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 const SignOut = () => {
-  return <div>SignOut</div>;
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: "Logout Successful" });
+  };
+  return (
+    <SignOutButton>
+      <Link
+        href="/"
+        className="w-full text-left capitalize"
+        onClick={handleLogout}
+      >
+        logout
+      </Link>
+    </SignOutButton>
+  );
 };
 export default SignOut;
