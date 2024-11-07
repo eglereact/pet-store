49333d3c49fb4b8d64895458b9b6ac0ca32f0df0
eglereact <eglereactjs@gmail.com>
2024-11-07 15:53:15 +0200

    product schema

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,6 +3,7 @@
 import db from "@/utils/db";
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs/server";
+import { productSchema } from "./schemas";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -58,24 +59,9 @@ export const createProductAction = async (
   const user = await getAuthUser();
 
   try {
-    const name = formData.get("name") as string;
-    const company = formData.get("company") as string;
-    const price = Number(formData.get("price") as string);
-    const image = formData.get("image") as File;
-    const description = formData.get("description") as string;
-    const featured = Boolean(formData.get("featured") as string);
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = productSchema.parse(rawData);
 
-    await db.product.create({
-      data: {
-        name,
-        company,
-        price,
-        image: "/images/hero1.jpg",
-        description,
-        featured,
-        clerkId: user.id,
-      },
-    });
     return { message: "product created" };
   } catch (error) {
     return renderError(error);
