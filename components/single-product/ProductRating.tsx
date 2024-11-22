c5a38a60bf50021ed39163aa224128c851dccb21
eglereact <eglereactjs@gmail.com>
2024-11-22 13:50:29 +0200

    dynamic review rating and count

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,10 +1,8 @@
+import { fetchProductRating } from "@/utils/actions";
 import { FaStar } from "react-icons/fa";
 
-//{ productId }: { productId: string }
-function ProductRating() {
-  //temp
-  const rating = 4.2;
-  const count = 25;
+async function ProductRating({ productId }: { productId: string }) {
+  const { rating, count } = await fetchProductRating(productId);
 
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
   const countValue = `(${count}) reviews`;
