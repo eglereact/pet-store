60b1f67fa6c107ef5a5fd7cce36b703858774bc5
eglereact <eglereactjs@gmail.com>
2024-11-25 12:36:08 +0200

    cart model

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { FaCartShopping } from "react-icons/fa6";
+import { fetchCartItems } from "@/utils/actions";
 
-const CartButton = () => {
+const CartButton = async () => {
+  const numItemsInCart = await fetchCartItems();
   return (
     <Button
       asChild
@@ -12,7 +14,7 @@ const CartButton = () => {
       <Link href="/cart">
         <FaCartShopping size={25} />
         <span className="absolute -top-1 right-1 bg-red-500 flex font-bold text-white rounded-full h-5 w-5 items-center justify-center text-sm">
-          9
+          {numItemsInCart}
         </span>
       </Link>
     </Button>
