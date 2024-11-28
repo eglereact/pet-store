2be742f17cd3b2b7ff7049d2f9975e2fd5e654a6
eglereact <eglereactjs@gmail.com>
2024-11-28 14:33:46 +0200

    cart finished

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -11,9 +11,9 @@ const CartPage = async () => {
   if (!userId) redirect("/");
 
   const previousCart = await fetchOrCreateCart({ userId });
-  const cart = await updateCart(previousCart);
+  const { currentCart, cartItems } = await updateCart(previousCart);
 
-  if (cart.numItemsInCart === 0) {
+  if (currentCart.numItemsInCart === 0) {
     return <SectionTitle text="empty cart" />;
   }
 
@@ -22,10 +22,10 @@ const CartPage = async () => {
       <SectionTitle text="shopping cart" />
       <div className="mt-8 grid gap-4 lg:grid-cols-12">
         <div className="lg:col-span-8">
-          <CartItemsList cartItems={cart.cartItems} />
+          <CartItemsList cartItems={cartItems} />
         </div>
         <div className="lg:col-span-4">
-          <CartTotals cart={cart} />
+          <CartTotals cart={currentCart} />
         </div>
       </div>
     </>
