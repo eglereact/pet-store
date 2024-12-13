0d0f4eeb3b446fbffb900e3dbd050f68d1d0c257
eglereact <eglereactjs@gmail.com>
2024-12-13 13:43:41 +0200

    more payment info style

--- components/global/TestNumbersBox.tsx
+++ components/global/TestNumbersBox.tsx
@@ -1,22 +1,24 @@
 function TestNumbersBox() {
   return (
-    <div>
-      <p className="text-xl font-bold -mt-14">
-        To test payments, use the following card details:
-      </p>
-      <ul>
-        <li>
-          <strong>Card Number:</strong> <span>4242 4242 4242 4242</span>
-        </li>
-        <li>
-          <strong>CVV:</strong> Any 3 digits (e.g., <span>123</span>)
-        </li>
-        <li>
-          <strong>Expiration Date:</strong> Any future date (e.g.,{" "}
-          <span>12/25</span>)
-        </li>
-      </ul>
-      <p className="my-4">
+    <div className="rounded-xl border p-4 -mt-10 mb-4">
+      <div>
+        <p className="text-xl font-bold">
+          To test payments, use the following card details:
+        </p>
+        <ul>
+          <li>
+            <strong>Card Number:</strong> <span>4242 4242 4242 4242</span>
+          </li>
+          <li>
+            <strong>CVV:</strong> Any 3 digits (e.g., <span>123</span>)
+          </li>
+          <li>
+            <strong>Expiration Date:</strong> Any future date (e.g.,{" "}
+            <span>12/25</span>)
+          </li>
+        </ul>
+      </div>
+      <p className="mt-4">
         Need more test card numbers?{" "}
         <a
           href="https://docs.stripe.com/testing"
